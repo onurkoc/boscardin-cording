f6d4f4b0b4efa630e26996d733fe0f0cb011c2b6
onurkoc <onur.koc@outlook.com>
2019-01-22 15:51:52 +0100

    server = app.server

--- app.py
+++ app.py
@@ -12,6 +12,8 @@ external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
+server = app.server
+
 app.layout = html.Div([
     html.Div([
         dcc.Upload(
